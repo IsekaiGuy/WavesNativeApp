4b2c6f00cf07ad0d3459df19327dfa78f09710e0
Andrey Kruglik <isekaiguy@protonmail.com>
2022-06-08 20:03:19 +0700

    Shadows and buttons added

--- src/Components/Player.tsx
+++ src/Components/Player.tsx
@@ -2,6 +2,8 @@ import React, {useState} from "react";
 import {Text, View, StyleSheet, TouchableOpacity} from "react-native";
 import Slider from "@react-native-community/slider";
 import { MaterialIcons } from '@expo/vector-icons';
+import { Shadow } from 'react-native-shadow-2';
+import {opacity} from "react-native-reanimated/lib/types/lib/reanimated2";
 
 interface IPlayer {
     audio: string,
@@ -11,6 +13,35 @@ interface IPlayer {
 
 const Player = (props: IPlayer) => {
     const [play, setPlay] = useState(false);
+
+    const styles = StyleSheet.create({
+        button: {
+            padding: 15,
+            borderRadius: 50,
+            color: `${props.color[0]}`,
+            shadowColor: 'grey',
+            shadowRadius: 1,
+            shadowOffset: {width: 10, height: 1},
+        },
+        controls: {
+            alignItems: 'center',
+            marginTop: 30,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            marginHorizontal: 50
+        },
+        durationData: {
+            flexDirection: 'row',
+            justifyContent: "space-between",
+            paddingHorizontal: 15
+        },
+        progressBar: {
+            width: 340,
+            height: 32,
+            flexDirection: 'row',
+            marginTop: 50,
+        },
+    })
 
     return (
         <View>
@@ -36,54 +67,51 @@ const Player = (props: IPlayer) => {
 
             <View style={styles.controls}>
                 <TouchableOpacity onPress={() => props.skipTrackHandler("skip-back")}>
-                    <MaterialIcons
-                        style={styles.button}
-                        name="skip-previous"
-                        size={24} color="black"
-                    />
+                    <Shadow
+                        distance={3}
+                        startColor={props.color[0]}
+                        radius={50}
+                        containerViewStyle={{opacity: 0.7}}
+                    >
+                        <MaterialIcons
+                            style={styles.button}
+                            name="skip-previous"
+                            size={24} color="black"
+                        />
+                    </Shadow>
                 </TouchableOpacity>
                 <TouchableOpacity>
-                    <MaterialIcons
-                        style={styles.button}
-                        name={play ? "pause" : 'play-arrow'}
-                        size={24} color="black"
-                    />
+                    <Shadow
+                        distance={3}
+                        startColor={props.color[0]}
+                        radius={50}
+                        containerViewStyle={{opacity: 0.7}}
+                    >
+                        <MaterialIcons
+                            style={styles.button}
+                            name={play ? "pause" : 'play-arrow'}
+                            size={36}
+                            color="black"
+                        />
+                    </Shadow>
                 </TouchableOpacity>
                 <TouchableOpacity onPress={() => props.skipTrackHandler("skip-forward")}>
-                    <MaterialIcons
-                        style={styles.button}
-                        name="skip-next"
-                        size={24} color="black"
-                    />
+                    <Shadow
+                        distance={3}
+                        startColor={props.color[0]}
+                        radius={50}
+                        containerViewStyle={{opacity: 0.7}}
+                    >
+                        <MaterialIcons
+                            style={styles.button}
+                            name="skip-next"
+                            size={24} color="black"
+                        />
+                    </Shadow>
                 </TouchableOpacity>
             </View>
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    button: {
-      // padding: 30,
-      // borderRadius: 50,
-      // color: 'rgba(201, 215, 230, 0.8)'
-    },
-    controls: {
-      marginTop: 30,
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      marginHorizontal: 70
-    },
-    durationData: {
-      flexDirection: 'row',
-      justifyContent: "space-between",
-      paddingHorizontal: 15
-    },
-    progressBar: {
-        width: 340,
-        height: 32,
-        flexDirection: 'row',
-        marginTop: 50,
-    }
-})
 
 export default Player;
